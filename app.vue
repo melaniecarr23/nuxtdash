1ab5807f5b7ff673d6ab45bf85a58ddf051142fe
melaniecarr23 <melaniecarr23@protonmail.com>
2024-08-04 23:47:07 -0400

    grab public dash data on app load

--- app.vue
+++ app.vue
@@ -5,4 +5,25 @@
 </template>
 
 <script setup>
+import { onMounted } from 'vue'
+
+import { useDashStore } from '~/stores/dashStore.ts';
+const dashStore = useDashStore()
+const { $customFetch } = useNuxtApp();
+
+onMounted(async () => {
+  try {
+    console.log('Fetching calendar data');
+    const data = await $customFetch('/calendar');
+    if (data) {
+      console.log('Saving calendar data');
+      dashStore.setCalendarData(data);
+    } else {
+      console.error('No data returned from API');
+    }
+
+  } catch (err) {
+    console.error('Error fetching calendar data:', err);
+  }
+});
 </script>
